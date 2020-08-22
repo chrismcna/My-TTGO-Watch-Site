6a562d6f8347a0474b3ca202aaef6b0a210d2ac8
chrismcna <chris.mcna@gmail.com>
2020-08-22 13:11:23 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ export default  props => {
             My-TTGO-Watch is an open source personal project that is slowly gaining a community behind it.
             
             <h3>Credits</h3>
-            A big thank you to Dirk Broßwick (aka sharandac) for all his hard work, it wouldnt have been possible with out you
+            A big thank you to Dirk Broßwick (aka sharandac) for all his hard work, it wouldnt have been possible with out him
         </>
     );
 }
