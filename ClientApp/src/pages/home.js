64bb37b327c533718bf1460643268b6ed26e13b1
chrismcna <chris.mcna@gmail.com>
2020-08-28 19:25:37 +0100

    improvements

--- ClientApp/src/pages/home.js
+++ ClientApp/src/pages/home.js
@@ -17,20 +17,23 @@ export default  props => {
 
             <h3>Features</h3>
 
-            Wifi  <br/>
-
-            Bluetooth - Gadgetbridge (Notification from mobile), remote wifi and weather app configuration, BLE characteristics (manufacturer name, firmware revision, power level, power state and more to come)  <br/>
-
-            Step Counter <br/>
-
-            Power management <br/>
-
-            Display <br/>
-
-            Time <br/>
-
             Apps - <Link to="/user-guide/apps/weather">Weather</Link>, <Link to="/user-guide/apps/stop-watch">StopWatch</Link>, <Link to="/user-guide/apps/crypto-ticker">Crypto Ticker</Link>, <Link to="/user-guide/apps/alexa-smart-home-controller">Alexa Smart Home Controller</Link> (in developement) and more to come <br/>
 
+            <Link to="/user-guide/gadget-bridge">Gadgetbridge</Link> Gadgetbridge - notifications from Whatsapps, Gmail, OsmAnd, Instagram... <br/>
+
+            <Link to="/user-guide/settings/wifi">Wifi</Link> <br/>
+
+            <Link to="/user-guide/settings/bluetooth">Bluetooth</Link> - remote <Link to="/device/bluetooth">wifi</Link> and <Link to="/device/apps/weather">weather app</Link> configuration, BLE characteristics (manufacturer name, firmware revision, power level, power state and more to come)  <br/>
+
+            <Link to="/user-guide/settings/steps">Step Counter</Link> <br/>
+
+            <Link to="/user-guide/settings/power">Power management</Link> <br/>
+
+            <Link to="/user-guide/settings/display">Display</Link> <br/>
+
+            <Link to="/user-guide/settings/time">Time</Link> <br/>
+
+            
             WebServer <br/>
 
 
--- ClientApp/src/pages/user-guide/gadget-bridge.js
+++ ClientApp/src/pages/user-guide/gadget-bridge.js
@@ -1,13 +1,53 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 
-export default  props => {
+
+export default props => {
     return (
         <>
             <h2>Gadgetbridge</h2>
 
-            <a href="https://gadgetbridge.org/" target="_blank"  rel="noopener noreferrer">Gadgetbridge (https://gadgetbridge.org/)</a> notification from mobile
-           
+            <p>
+                <a href="https://gadgetbridge.org/" target="_blank" rel="noopener noreferrer">Gadgetbridge (https://gadgetbridge.org/)</a> notification from mobile and more to come later.
+            </p>
+
+            <p>
+                Supports:
+                <ul>
+                    <li>Whatsapp</li>
+                    <li>K-9 Mail</li>
+                    <li>Gmail</li>
+                    <li>E-Mail</li>
+                    <li>OsmAnd</li>
+                    <li>YouTube</li>
+                    <li>Instagram</li>
+                    <li>Tinder</li>
+                </ul>
+
+            </p>
+
+
+            <h3>Setup</h3>
+
+            <p>
+                The setup is fairly simple<br/>
+                <ol>
+                    <li>Install Gadgetbridge on your Android deivce - accepting all the permissions</li>
+                    <li>Open Gadgetbridge app</li>
+                    <li>Click the add button</li>
+                    <li>Click Start Discovery</li>
+                    <li>Select your watch once found</li>
+                    <li>Enter the code from the watch screen in to Gadgetbridge app</li>
+                    <li>Done</li>
+                </ol>
+            </p>
+
+            <h3>Issue</h3>
+
+            <p>
+                Not seeing the watch? have you made the watch discoverable? <Link to="/user-guide/setting/bluetooth">bluetooth user guide</Link>
+            </p>
         </>
     );
 }
